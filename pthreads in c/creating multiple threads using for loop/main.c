2523432feb1e663b5b44b855e0e6cffbb9cb6c5a
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-09-06 02:33:38 -0400

    Create main.c

---
+++ pthreads in c/creating multiple threads using for loop/main.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : structures.c
+ Author      : Amonjot
+ Version     :
+ Copyright   : Your copyright notice
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+int a = 0;
+int b = 0 ;
+void *func( void *var){
+
+    a = 1;
+    b = 1;
+    printf("value of A from THREAD 1 is %d\n",a);
+    printf("value of B from THREAD 1 is %d\n",b);    
+}
+void *func1( void *var1){
+     a = 2;
+     b = 2;
+    printf("value of A from THREAD 2 is %d\n",a);
+    printf("value of B from THREAD 2 is %d\n",b);    
+     
+}
+
+int main(void) {
+    pthread_t thread1;
+    pthread_t thread2;
+    for(int i= 0; i<10 ; i++){
+        sleep(1.5);
+        pthread_create(&thread1, NULL, func,NULL);
+        pthread_create(&thread2, NULL, func1, NULL);
+        pthread_join(thread1,NULL);
+        pthread_join(thread2,NULL);
+    } 
+    return EXIT_SUCCESS;
+}
