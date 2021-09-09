62e7e450d65da6b731a92aae2eca3486a3e5d5f7
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-09-09 03:31:41 -0400

    Update main.c

--- pthreads in c/ACCESSING ATTRIBUTES/scheduling the attributes with priority/main.c
+++ pthreads in c/ACCESSING ATTRIBUTES/scheduling the attributes with priority/main.c
@@ -1,1 +1,18 @@
-hello
+
+  pthread_t THREAD1, THREAD2;
+  pthread_attr_t TH1,TH2;
+  pthread_attr_init(&TH1);
+  pthread_attr_init(&TH2);
+  sched_param param1,param2;
+  pthrad_attr_getparam(&TH1,&param1);
+  pthrad_attr_getparam(&TH2,&param2);
+  param1.sched_priority = 1;
+  param2.sched_priority = 2;
+  pthread_attr_setparam(&TH1,&param1);
+  pthread_attr_setparam(&TH1,&param1);
+
+  
+  
+
+
+
